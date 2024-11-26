754ff03419c68abac8bcdd072fbe092724574bb2
SHG2021182020 <96454074+SHG2021182020@users.noreply.github.com>
2024-11-26 16:52:46 +0900

    쓸데업는거제거

--- Game/Character.py
+++ Game/Character.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aff0e043c0cd28a178c37b4caf51f7773f0737d49fee75ac8763cb57b2f103ed
-size 11365
+oid sha256:ebc6560debfcfa18cd5549fb22c78c938c7b22648190d460dddfb56c4878419a
+size 11507
