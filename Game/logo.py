a960540fa5b3f2462f04eee00c7384b957d7cb2b
SHG2021182020 <96454074+SHG2021182020@users.noreply.github.com>
2024-12-02 19:18:35 +0900

    로고 수정

--- Game/logo.py
+++ Game/logo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e728d39ae46af14802108d764a955d3d47b33f938ced836b67d8d6ba9d1480f9
-size 384
+oid sha256:c5fa8aa3f255446eb636cdd48592764db819230918551c929927a8ed004ec7a4
+size 517
--- Game/play_mode.py
+++ Game/play_mode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82a49b9822ff2e34b88bd9ac1bf6f7d5e2e7a1df72c16a8539b570f291f596c5
-size 4524
+oid sha256:79da64493e5929d6db5a85f246de47544bc519b57a4f6b14142ecdf068e711e7
+size 4569
